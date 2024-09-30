e2334580fb9100e598a14eb113e3de05c8b60b41
Josué Díaz (JosueDM94) <joem.dima@outlook.com>
2024-09-30 15:25:49 -0600

    .NET MAUI Blazor Hybrid App

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MauiGithubActions;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new MainPage();
+	}
+}
+
---
+++ MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MauiGithubActions;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+}
+
